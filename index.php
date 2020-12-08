0123bf765ece14a5ae531879ed88be2339e84afa
Max <maxblanck@outlook.fr>
2020-12-08 16:29:33 +0100

    php Partie 4  Exercice 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    function truth()
+    {
+        return true;
+    };
+    ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=, initial-scale=1.0" />
+    <title>Exercice 1 Partie 4</title>
+</head>
+
+<body>
+    <h1>Exercice 1 Partie 4</h1>
+</body>
+
+</html>
